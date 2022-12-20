907879609dcaf3b288ae30ee2147b3b86b1eec66
umerfarooq91 <83349246+umerfarooq91@users.noreply.github.com>
2022-12-21 00:27:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//hex code conersion
+const getColor = () =>{
+    const randomNumber =Math.floor( Math.random()*16777215);
+    const randomCode = "#" + randomNumber.toString(16); 
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+    //to put color code on clipboard
+    navigator.clipboard.writeText(randomCode)
+}
+//event call
+document.getElementById("btn").addEventListener(
+    "click",
+     getColor
+)
+//initial call
+getColor();
